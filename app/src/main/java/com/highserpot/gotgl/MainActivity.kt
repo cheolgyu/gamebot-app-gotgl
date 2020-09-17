8c631b694b083d93b14f5b37da5d76abb24332c3
cheolgyu <rb7480@knou.ac.kr>
2020-09-17 20:14:12 +0900

    initial commit

---
+++ app/src/main/java/com/highserpot/gotgl/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.highserpot.gotgl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.highserpot.background.MediaProjectionActivity
+
+class MainActivity : MediaProjectionActivity() {
+
+}
